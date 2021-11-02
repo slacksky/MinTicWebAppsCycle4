892bdbc87c91e1686e242db4a7b18132fb927d51
slacksky <slacksky@gmail.com>
2021-11-02 11:07:27 -0500

    user connection?, error router.post('/user', (req, res) => {     let newUser = new UserSchema({         nombre: req.body.nombre,         apellidos: req.body.apellidos,         correo: req.body.correo,         telefono: req.body.telefono,         password: req.body.password,     })     newUser.save(function(err, data){         if(err){             console.log(err)         }else{         res.send("saved successfully " + newUser)         }     }); })

--- index.js
+++ index.js
@@ -3,9 +3,39 @@ const app = express()
 const port = process.env.PORT || 3000;
 const mongoose = require('mongoose')
 const router = express.Router();
+//user schema improt from models folder
+const UserSchema = require('./models/User.js')
+//mongo db connection command
+mongoose.connect("mongodb+srv://sky:JV46pt27.@clustertic.wzb4v.mongodb.net/test")
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 router.get('/', (req, res) => {
-    res.send("Hello World");
+    UserSchema.find(function(err, data){
+        if(err){
+            console.log(err)
+        }else{
+            res.send(data)
+        }
+    })
+
 });
+router.post('/user', (req, res) => {
+    let newUser = new UserSchema({
+        nombre: req.body.nombre,
+        apellidos: req.body.apellidos,
+        correo: req.body.correo,
+        telefono: req.body.telefono,
+        password: req.body.password,
+    })
+    newUser.save(function(err, data){
+        if(err){
+            console.log(err)
+        }else{
+        res.send("saved successfully " + newUser)
+        }
+    });
+})
+
 app.use(router)
 app.listen(port,() => {
     console.log('Listening on '+port)
